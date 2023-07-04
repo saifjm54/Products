043368b4900a5485b565dd9b6a8d004635d615bd
saifjm54 <saifjmaa54@gmail.com>
2023-07-04 15:10:39 +0100

    A tested version of the product module with jdbc

---
+++ src/test/java/com/example/product/ProductRowMapperTest.java
@@ -0,0 +1,31 @@
+package com.example.product;
+
+import com.example.product.model.Product;
+import com.example.product.repository.ProductRowMapper;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ProductRowMapperTest {
+
+    @Test
+    void mapRow() throws SQLException {
+        ProductRowMapper productRowMapper = new ProductRowMapper();
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.getInt("id")).thenReturn(1);
+        when(resultSet.getString("code")).thenReturn("EPARLOG");
+        when(resultSet.getString("name")).thenReturn("Epargne Logement");
+        Product actual = productRowMapper.mapRow(resultSet,1);
+        Product expected = new Product(
+                1,
+                "EPARLOG",
+                "Epargne Logement"
+
+        );
+        assertThat(actual).isEqualTo(expected);
+    }
+}
